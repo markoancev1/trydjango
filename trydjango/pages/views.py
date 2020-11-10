c0f07e098e41d711626b25b47e591baec165c967
marko <ancev.marko@yahoo.com>
2020-11-10 17:29:46 +0100

    02. For Loop in a Template

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -8,4 +8,11 @@ def home_view(request, *args, **kwargs):
 def contact_view(request, *args, **kwargs):
     return render(request, "Contact.html", {})
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+
+    my_context = {
+        "my_text": "This is a text",
+        'my_number': 123,
+        "my_list": [123, 456, 789, 'ABC']
+    }
+
+    return render(request, "about.html", my_context)

de5afbbbb4664e4905145cb3bac2666b6f6f8899
marko <ancev.marko@yahoo.com>
2020-11-11 21:49:29 +0100

    07. Delete and Confirm

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.http import Http404
 
@@ -49,3 +49,16 @@ def dynamic_lookup_view(request, id):
         'object': obj
     }
     return render(request, 'product/detail.html', context)
+
+
+def product_delete_view(request, id):
+
+    obj = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('../../')
+    context = {
+        'object': obj
+    }
+
+    return render(request, 'product/delete.html', context)
--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 
 
 from pages.views import home_view, contact_view, about_view
-from products.views import product_detail_view, product_create_view, dynamic_lookup_view
+from products.views import product_detail_view, product_create_view, product_delete_view, dynamic_lookup_view
 
 urlpatterns = [
     path('', home_view),
     path('product/<int:id>/', dynamic_lookup_view, name='product'),
+    path('product/<int:id>/delete/', product_delete_view, name='product-delete'),
     path('contact/', contact_view),
     path('about/', about_view),
     path('admin/', admin.site.urls),
